e1921e66098541302f8d6c061c109b08f13800b3
drumath <2237k.drumath@gmail.com>
2024-10-09 02:29:58 +0900

    :sparkles: Unityシーン2を作成

---
+++ unity-sample/Assets/App/Scripts/MoveCameraBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MoveCameraBehaviour : MonoBehaviour
+{
+    private const float MovingSpeed = 0.2f;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * MovingSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * MovingSpeed;
+        }
+    }
+}
